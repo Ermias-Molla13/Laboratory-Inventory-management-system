ff9e1fbc4c68893356f04214cd68ae1c0b84c796
muhafuad <131243901+muhafuad@users.noreply.github.com>
2025-12-25 08:50:40 +0000

    Add Chemical entity with properties and methods

--- backend/src/main/java/org/wldu/webservices/enities/Chemical.java
+++ backend/src/main/java/org/wldu/webservices/enities/Chemical.java
@@ -9,4 +9,112 @@ import java.time.LocalDate;
 @Entity
 @Table(name ="chemicals")
 public class Chemical {
+  package org.wldu.webservices.enities;
+
+import jakarta.persistence.*;
+import org.springframework.format.annotation.DurationFormat;
+import java.time.LocalDate;
+
+
+
+@Entity
+@Table(name ="chemicals")
+public class Chemical {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private  String chemicalFormula;
+
+    @Column(nullable = false)
+    private double quantity;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Unit unit;
+
+    @Column(nullable = false)
+    private LocalDate expiryDate;
+
+    @Column(nullable = false)
+    private String storageLocation;
+
+
+
+
+    public Chemical(){}
+
+    public Chemical(String name, String chemicalFormula, double quantity,
+                    Unit unit, LocalDate expiryDate, String storageLocation) {
+
+        this.name = name;
+        this.chemicalFormula = chemicalFormula;
+        this.quantity = quantity;
+        this.unit = unit;
+        this.expiryDate = expiryDate;
+        this.storageLocation = storageLocation;
+    }
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getChemicalFormula() {
+        return chemicalFormula;
+    }
+
+    public void setChemicalFormula(String chemicalFormula) {
+        this.chemicalFormula = chemicalFormula;
+    }
+
+    public double getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(double quantity) {
+        this.quantity = quantity;
+    }
+
+    public Unit getUnit() {
+        return unit;
+    }
+
+    public void setUnit(Unit unit) {
+        this.unit = unit;
+    }
+
+    public LocalDate getExpiryDate() {
+        return expiryDate;
+    }
+
+    public void setExpiryDate(LocalDate expiryDate) {
+        this.expiryDate = expiryDate;
+    }
+
+    public String getStorageLocation() {
+        return storageLocation;
+    }
+
+    public void setStorageLocation(String storageLocation) {
+        this.storageLocation = storageLocation;
+    }
+}
+
 }
