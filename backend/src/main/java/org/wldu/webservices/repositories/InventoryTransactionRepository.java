9ade6dee287ffe14dafbf63306f2f23959156400
Ermias-Molla13 <ermiasmolla785@gmail.com>
2026-01-01 07:22:42 +0300

    remove comments InventoryTransactionRepository.java

--- backend/src/main/java/org/wldu/webservices/repositories/InventoryTransactionRepository.java
+++ backend/src/main/java/org/wldu/webservices/repositories/InventoryTransactionRepository.java
@@ -11,18 +11,18 @@ import java.util.List;
 @Repository
 public interface InventoryTransactionRepository extends JpaRepository<InventoryTransaction, Long> {
 
-    // Get all transactions for a specific equipment
+    
     List<InventoryTransaction> findByEquipment_Id(Long equipmentId);
 
-    // Get all transactions for a specific chemical
+    
     List<InventoryTransaction> findByChemical_Id(Long chemicalId);
 
-    // Get the latest 5 transactions by transaction date
+    
     List<InventoryTransaction> findTop5ByOrderByTransactionDateDesc();
 
-    // Get all transactions between two dates
+    
     List<InventoryTransaction> findByTransactionDateBetween(LocalDate startDate, LocalDate endDate);
 
-    // Get all transactions by type (enum)
+    
     List<InventoryTransaction> findByTransactionType(TransactionType transactionType);
 }
